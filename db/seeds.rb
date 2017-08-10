5b9e261d1ab6bef812b576b1e7cdbda1b9c7f038
Adrian Hawrylak <adrianhawrylak@MacBook-Air-Adrian.local>
2017-08-10 17:31:24 +0200

    FT-1 Add basic user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Jon', last_name: 'Snow', email: 'a@example.com', password: 'qweqweqwe')
+User.create(first_name: 'Hot', last_name: 'Pie', email: 'b@example.com', password: 'qweqweqwe')
