877cf62c6573c5626159ec8337a230fa92eabe83
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-08-11 13:21:49 +0200

    Latest page should have 6 images spec

--- spec/features/latest_pagination_spec.rb
+++ spec/features/latest_pagination_spec.rb
@@ -4,17 +4,17 @@ require 'database_cleaner'
 describe 'Landing page' do
 
   before do
-    user = User.create!(first_name: 'Jon', email: 'admin@admin.com',
+    @user = User.create!(first_name: 'Jon', email: 'admin@admin.com',
                     password: 'secretsecret', password_confirmation: 'secretsecret',
                     avatar: File.new("#{Rails.root}/app/assets/images/jon_snow.jpg"))
-    20.times do
-      Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
-                 description: 'Fancy description', user: user)
-    end
   end
 
-  context 'User visits latest page' do
+  context 'User visits latest page with 3 pages' do
     before do
+      20.times do
+      Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
+                 description: 'Fancy description', user: @user)
+      end
       visit latest_path 
     end
     it 'should have pagination section' do
@@ -22,6 +22,19 @@ describe 'Landing page' do
     end
     it 'should have 8 images' do
       expect(page).to have_selector(:css, 'div.single-picture', count: 8)
+    end
+  end
+
+  context 'User visits latest page with 6 pictures' do
+    before do
+      6.times do
+      Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
+                 description: 'Fancy description', user: @user)
+      end
+      visit latest_path 
+    end
+    it 'should have 6 images' do
+      expect(page).to have_selector(:css, 'div.single-picture', count: 6)
     end
   end
 end
