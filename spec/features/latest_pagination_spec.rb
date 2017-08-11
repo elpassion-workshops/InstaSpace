43884e12855b8081d380445575fc6b14e7281d05
Adrian Hawrylak <adrianhawrylak@MacBook-Air-Adrian.local>
2017-08-11 12:45:08 +0200

    Add latest picture pagination spec

---
+++ spec/features/latest_pagination_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Latest pagination' do
+  let(:user_password) {"qweqweqwe"}
+  let!(:user) { User.create(first_name: 'Jon', last_name: 'Snow', email: "a@example.com", password: user_password) }
+
+  context 'with 2 pictures in database' do
+    let!(:pictures) do
+      2.times do
+        Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
+                       description: 'Fancy description', user: user)
+      end
+    end
+
+    scenario 'user can see 2 pictures and pagination' do
+      visit latest_path
+      expect(page).to have_selector('div.single-picture', count: 2)
+      expect(page).to have_selector('div.pagination', count: 2)
+    end
+  end
+
+  context 'with 10 pictures in database' do
+    let!(:pictures) do
+      10.times do
+        Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
+                       description: 'Fancy description', user: user)
+      end
+    end
+
+    scenario 'user can see 8 pictures and pagination' do
+      visit latest_path
+      expect(page).to have_selector('div.single-picture', count: 8)
+      expect(page).to have_selector('div.pagination', count: 2)
+    end
+  end
+end
