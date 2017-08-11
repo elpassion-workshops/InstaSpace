99db4c8aa55851717f4d880d2e8598eb52209c17
Adrian Hawrylak <adrianhawrylak@MacBook-Air-Adrian.local>
2017-08-11 14:01:27 +0200

    Adapt pagination specs for load more

--- spec/features/latest_pagination_spec.rb
+++ spec/features/latest_pagination_spec.rb
@@ -5,7 +5,7 @@ describe 'Latest pagination' do
   let!(:user) { User.create(first_name: 'Jon', last_name: 'Snow', email: "a@example.com", password: user_password) }
 
   context 'with 2 pictures in database' do
-    let!(:pictures) do
+    before do
       2.times do
         Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
                        description: 'Fancy description', user: user)
@@ -15,26 +15,30 @@ describe 'Latest pagination' do
     scenario 'user can see 2 pictures' do
       visit latest_path
       expect(page).to have_selector('div.single-picture', count: 2)
+      expect(page).to_not have_link('More')
     end
+
+
   end
 
   context 'with 10 pictures in database' do
-    let!(:pictures) do
-      10.times do
+    before do
+      20.times do
         Picture.create(file: File.new("#{Rails.root}/app/assets/images/picture#{rand(1..5)}.jpeg"),
                        description: 'Fancy description', user: user)
       end
     end
 
-    scenario 'user can see 8 pictures' do
+    scenario 'user clicks load more' do
       visit latest_path
       expect(page).to have_selector('div.single-picture', count: 8)
-    end
-
-    scenario 'user clicks next page' do
-      visit latest_path
-      click_link_or_button 'Next'
-      expect(page).to have_selector('div.single-picture', count: 2)
+      expect(page).to have_link('More')
+      click_link_or_button 'More'
+      expect(page).to have_selector('div.single-picture', count: 16)
+      expect(page).to have_link('More')
+      click_link_or_button 'More'
+      expect(page).to have_selector('div.single-picture', count: 20)
+      expect(page).to_not have_link('More')
     end
   end
 end
