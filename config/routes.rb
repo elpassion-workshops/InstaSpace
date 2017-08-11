348e16cad3706c09f7739863f24971af924dafb5
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-08-11 12:59:23 +0200

    Prepare routes for latest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
   devise_for :users
-  get '/latest' => 'pictures#latest'
+  get '/latest' => 'pictures#latest', as: 'latest'
   get '/pictures' => 'pictures#latest'
   resources :pictures
 end
